7c19bd0687dba37a169b1f1b3ee406b1f40fa74c
Ethan <robewilkes@yahoo.com>
2021-06-21 15:43:41 -0500

    fixed crossOrigin error in index

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"></link>
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" 
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"/>
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossOrigin="anonymous"/>
         <link
         rel="stylesheet"
         type="text/css"
